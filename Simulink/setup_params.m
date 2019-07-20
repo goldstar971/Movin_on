cf3dc8f5016e0e608827b8e85fe43b76a4665763
cadis <matthewdgould1@gmail.com>
2019-07-20 14:40:36 -0400

    added Simulink project for modeling FOC algorithm modified motor control pcb notes and BOM to use 4 330UF aluminum electrolytic capacitors (EEE-FT1H331AP) in place of 2 680 UF capacitors (EEV-FK1J681M) to keep in height limits added model of EEE-FT1H331AP removed Model of EEV-FK1J681M resetup DSC coding project

---
+++ Simulink/setup_params.m
@@ -0,0 +1,8 @@
+L=.299; % stator inductance
+R=.065; %rotor resistance
+J=3; %torque coefficient
+P=14; %number of magnetic pole pairs
+phi=3;%permanent magnetic flux
+KV=130; % motor KV rating
+ts=1/20000; % switching frequency
+ob
